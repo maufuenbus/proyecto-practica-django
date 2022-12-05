9bda31c0db7086c6411f3d69425de9d6a87f99da
Mauricio fuentes <mau.fuentesb@duocuc.cl>
2022-12-05 12:44:01 -0300

    .

--- app/urls.py
+++ app/urls.py
@@ -4,12 +4,12 @@ from .views import index, crucigrama, eva_param_func, eva_param_text, memorama, 
 
 urlpatterns = [
     path('', index, name="index"),
-    path('memorama/', memorama, name="memorama"),
-    path('crucigrama/', crucigrama, name="crucigrama"),
+    #path('memorama/', memorama, name="memorama"),
+    #path('crucigrama/', crucigrama, name="crucigrama"),
     path('vocalizacion/', VocalizacionView.as_view(), name="vocalizacion"),
+    #path('intensidad/', intensidad, name="intensidad"),
     path('intensidad/', intensidad, name="intensidad"),
-    path('oscilograma/', oscilograma, name="oscilograma"),
-    path('medidor/', medidor, name="medidor"),
-    path('eva_param_func/', eva_param_func, name="eva_param_func"),
-    path('eva_param_text/', eva_param_text, name="eva_param_text"),
+    #path('medidor/', medidor, name="medidor"),
+    #path('eva_param_func/', eva_param_func, name="eva_param_func"),
+    #path('eva_param_text/', eva_param_text, name="eva_param_text"),
 ]
